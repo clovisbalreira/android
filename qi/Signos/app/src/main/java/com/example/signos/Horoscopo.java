870e37abaf0857ebe96e7d8d0271c76a07909f67
Clóvis Balreira Rodrigues <62964663+clovisbalreira@users.noreply.github.com>
2022-09-17 20:17:43 -0300

    17-09-2022

--- qi/Signos/app/src/main/java/com/example/signos/Horoscopo.java
+++ qi/Signos/app/src/main/java/com/example/signos/Horoscopo.java
@@ -17,8 +17,7 @@ public class Horoscopo {
         return diaNasc;
     }
 
-    public void setDiaNasc(byte diaNasc) {
-        this.diaNasc = diaNasc;
+    public void setDiaNasc(byte diaNasc) { this.diaNasc = diaNasc;
     }
 
     public byte getMesNasc() {
@@ -34,6 +33,7 @@ public class Horoscopo {
     }
 
     public void setDiaAt(byte diaAt) {
+        diaAt = 16;
         this.diaAt = diaAt;
     }
 
@@ -65,23 +65,67 @@ public class Horoscopo {
         return ((this.getDiaNasc() >= 1 && this.getDiaNasc() <= 31) && (this.getAnoNasc() >= 0) &&
                 (this.getMesNasc() == 1 || this.getMesNasc() == 3 || this.getMesNasc() == 5 || this.getMesNasc() == 7 || this.getMesNasc() == 8 || this.getMesNasc() == 10 || this.getMesNasc() == 12)
                 || ((this.getMesNasc() == 4 || this.getMesNasc() == 6 || this.getMesNasc() == 9 || this.getMesNasc() == 11)  && this.getDiaNasc() <= 30) ||
-                (this.getMesNasc() == 2 && (this.getAnoNasc() % 4 == 0 && ( this.getAnoNasc() % 100 != 0 || this.getAnoNasc() % 400 == 0 ) && this.getDiaNasc() == 29) || this.getDiaNasc() == 28));
+                (this.getMesNasc() == 2 && (this.getAnoNasc() % 4 == 0 && ( this.getAnoNasc() % 100 != 0 || this.getAnoNasc() % 400 == 0 ) && this.getDiaNasc() == 29) || this.getDiaNasc() <= 28));
     }
 
     public String mostrarSigno(){
-        return "";
+        if(validarData()){
+            if(this.getDiaNasc() >= 20 && this.getMesNasc() == 3 || this.getDiaNasc() <= 20 && this.getMesNasc() == 4){
+                return "Áries";
+            }else if(this.getDiaNasc() >= 21 && this.getMesNasc() == 4 || this.getDiaNasc() <= 20 && this.getMesNasc() == 5){
+                return "Touro";
+            }else if(this.getDiaNasc() >= 21 && this.getMesNasc() == 5 || this.getDiaNasc() <= 20 && this.getMesNasc() == 6){
+                return "Gêmeos";
+            }else if(this.getDiaNasc() >= 21 && this.getMesNasc() == 6 || this.getDiaNasc() <= 21 && this.getMesNasc() == 7){
+                return "Câncer";
+            }else if(this.getDiaNasc() >= 22 && this.getMesNasc() == 7 || this.getDiaNasc() <= 22 && this.getMesNasc() == 8){
+                return "Leão";
+            }else if(this.getDiaNasc() >= 23 && this.getMesNasc() == 8 || this.getDiaNasc() <= 22 && this.getMesNasc() == 9){
+                return "Virgem";
+            }else if(this.getDiaNasc() >= 23 && this.getMesNasc() == 9 || this.getDiaNasc() <= 22 && this.getMesNasc() == 10){
+                return "Libra";
+            }else if(this.getDiaNasc() >= 23 && this.getMesNasc() == 10 || this.getDiaNasc() <= 21 && this.getMesNasc() == 11){
+                return "Escorpião";
+            }else if(this.getDiaNasc() >= 22 && this.getMesNasc() == 11 || this.getDiaNasc() <= 21 && this.getMesNasc() == 12){
+                return "Sagitário";
+            }else if(this.getDiaNasc() >= 22 && this.getMesNasc() == 12 || this.getDiaNasc() <= 20 && this.getMesNasc() == 1){
+                return "Capricórnio";
+            }else if(this.getDiaNasc() >= 21 && this.getMesNasc() == 1 || this.getDiaNasc() <= 18 && this.getMesNasc() == 2){
+                return "Aquário";
+            }else{
+                return "Peixes";
+            }
+        }else{
+            return "";
+        }
     }
 
     public byte calcularIdade(){
-        return 1;
+        if(this.getMesAt() <= this.getMesNasc()){
+            if(this.getDiaAt() < this.getDiaNasc()){
+                return (byte) (this.getAnoAt() - this.getAnoNasc() - 1 );
+            }else{
+                return (byte) (this.getAnoAt() - this.getAnoNasc());
+            }
+        }else{
+            return (byte) (this.getAnoAt() - this.getAnoNasc());
+        }
     }
 
     public String parabenizar(){
-        return "";
+        if(this.getDiaAt() == this.getDiaNasc() && this.getMesAt() == this.getMesNasc()){
+            return "\nFeliz Aniversário!!!";
+        }else{
+            return "";
+        }
     }
 
     @Override
     public String toString() {
-        return this.getNome() + "você é do signo de " + this.mostrarSigno() + " e tem " + this.calcularIdade();
+        if(this.validarData()){
+            return this.validarData() +"   "+ this.getNome() + " você é do signo de " + this.mostrarSigno() + " e tem " + this.calcularIdade() + " anos." + parabenizar();
+        }else{
+            return "Data Inválida";
+        }
     }
 }
--- qi/Signos/app/src/main/java/com/example/signos/MainActivity.java
+++ qi/Signos/app/src/main/java/com/example/signos/MainActivity.java
@@ -4,15 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-
+    Horoscopo horoscopo;
+    EditText nome, dia, mes, ano;
+    TextView resposta, hoje;
+    /*SimpleDateFormat diaFormatado = new SimpleDateFormat("dd");
+    String diaAgora = diaFormatado.format(new Date());
+    SimpleDateFormat mesFormatado = new SimpleDateFormat("MM");
+    String mesAgora = mesFormatado.format(new Date());
+    SimpleDateFormat anoFormatado = new SimpleDateFormat("yyyy");
+    String anoAgora = anoFormatado.format(new Date());*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nome = findViewById(R.id.edtNome);
+        dia = findViewById(R.id.edtDia);
+        mes = findViewById(R.id.edtMes);
+        ano = findViewById(R.id.edtAno);
+        resposta = findViewById(R.id.txtResposta);
+        hoje = findViewById(R.id.txtHoje);
+        horoscopo = new Horoscopo();
+        //hoje.setText(diaAgora + " / " + mesAgora + " / " + anoAgora);
+    }
+
+    public void verificarCampo(EditText campo){
+        if(campo.getText().toString().isEmpty()){
+            campo.setError("Campo vazio");
+        }
     }
 
     public void mostrarSigno(View view) {
+        if(nome.getText().toString().isEmpty() || dia.getText().toString().isEmpty() || mes.getText().toString().isEmpty() || ano.getText().toString().isEmpty()){
+            verificarCampo(nome);
+            verificarCampo(dia);
+            verificarCampo(mes);
+            verificarCampo(ano);
+            Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+        }else{
+            /*horoscopo.setDiaAt(Byte.parseByte(diaAgora));
+            horoscopo.setMesAt(Byte.parseByte(mesAgora));
+            horoscopo.setAnoAt(Integer.parseInt(anoAgora));*/
+            horoscopo.setNome(nome.getText().toString());
+            horoscopo.setDiaNasc(Byte.parseByte(dia.getText().toString()));
+            horoscopo.setMesNasc(Byte.parseByte(mes.getText().toString()));
+            horoscopo.setAnoNasc(Integer.parseInt(ano.getText().toString()));
+            resposta.setText(horoscopo.toString());
+        }
     }
 }
